c206741529c51fabf813ac9417b32a02429afb31
QuangMinh <minhtruong20002@gmail.com>
2022-06-08 15:15:29 +0700

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,13 +10,5 @@ Rails.application.routes.draw do
   resources :roles
   resources :users
 
-  authenticated :user do
-    root to: 'pages#home', as: :authenticated_root
-  end
-
   root to: 'pages#home'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
