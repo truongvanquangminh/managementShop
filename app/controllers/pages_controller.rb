3dd3d2f0916a00b32983075e336bbd1091a3eeaf
QuangMinh <minhtruong20002@gmail.com>
2022-06-14 17:38:26 +0700

    Show product detail

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,11 @@ class PagesController < ApplicationController
     @q = Product.ransack(params[:q])
     @products = @q.result(distinct: true)
   end
+
+  def productDetail
+    @product = Product.find(params[:id])
+    if @product.nil?
+      redirect_to action: :index
+    end
+  end
 end
